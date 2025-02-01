fcf002b21d3fa6e0f885c0ed20dea47e54621470
Rishi Gurnani <rgur@Rishis-MacBook-Air-2.local>
2025-02-01 17:50:57 -0500

    first

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// js/script.js
+
+// Change navbar background on scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
